1ff1a3f80ee5ccf9c6889f7406fce746e0bfcbb6
kho903 <gmldnr2222@naver.com>
2021-01-21 09:29:18 +0900

    spring mvc

---
+++ board/src/exam/spring/board/config/WebAppInitializer.java
@@ -0,0 +1,23 @@
+package exam.spring.board.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {MvcConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
